642458062c3967b8165cc44bfe49b4a185165425
Blin Kukaj <kblin164@gmail.com>
2022-04-10 01:48:20 +0200

    Here we turn the number variable to an object and we can see that the object  type copy the reference so the function can do its work

--- 2nd-Part/app.js
+++ 2nd-Part/app.js
@@ -1,6 +1,32 @@
-let x = {value: 5};
-let y = x;
-x.value = 20;
- 
+// let x = {value: 5};
+// let y = x;
+// x.value = 20;
 
 // console.log(`y: ${y}, x: ${x}`);
+
+////////////////////////////////
+// By this code below we can understand that primitives are copied by their value
+// let number = 10;
+
+// function increase(number){
+//     number++
+// }
+
+// increase(number)
+// console.log(number);
+
+////////////////////////////////
+
+////////////////////////////////
+/*Here we turn the number variable to an object and we can see that the object 
+type copy the reference so the function can do its work */
+
+let number = { value: 10 };
+
+function increase(number) {
+    number.value++;
+  }
+
+
+increase(number);
+console.log(number);
