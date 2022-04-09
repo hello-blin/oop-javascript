c519e6ab73fd2db36d70d9fff77e20b2fc14e0a5
Blin Kukaj <kblin164@gmail.com>
2022-04-10 00:58:16 +0200

    Object literals

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hello');
+// !!!!! Object Literals !!!!!
+
+
+
+// const circle = {
+//     radius: 1,
+//     location: {
+//         x: 1,
+//         y: 1
+//     },
+//     draw: function(){
+//         console.log('Drawing');
+//     }
+// };
+
+// circle.draw();
+
+
+// -------- Object Literals -------
+
+
