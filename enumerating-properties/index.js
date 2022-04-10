0a2ff890bf81eb37bfd43fd5803ff9d1010c5ab5
Blin Kukaj <kblin164@gmail.com>
2022-04-10 02:08:50 +0200

    To iterate throguh an object we can use FOR loop, we can create a local variable inside and use keyword 'in' which represents every property inside the circle. We can log also the values of these members using brackets which we put in the key variable so it implements the property  inside and we can produce or show the value through this rule.

---
+++ enumerating-properties/index.js
@@ -0,0 +1,19 @@
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log("Drawww");
+  };
+}
+
+const circle = new Circle(10);
+/*To iterate throguh an object we can use FOR loop,
+we can create a local variable inside
+and use keyword 'in' which represents
+every property inside the circle.
+We can log also the values of these members using brackets which we put in the key variable so it implements the property 
+inside and we can produce or show the value
+through this rule.
+*/
+for (let key in circle) {
+  console.log(key, circle[key]);
+}
