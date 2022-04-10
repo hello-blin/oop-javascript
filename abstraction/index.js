70e3d4cec05ed1f9fbfa883211d6f6debffe38a8
Blin Kukaj <kblin164@gmail.com>
2022-04-10 04:01:14 +0200

    `z

--- abstraction/index.js
+++ abstraction/index.js
@@ -2,12 +2,17 @@ function Circle(radius) {
   this.radius = radius;
 
   let defaultLocation = { x: 0, y: 0 };
-  let computeOptimumLocation = function (factor) {};
-  
+
+  this.getDefaultLocation = function (){
+      return defaultLocation;
+  }
+
   this.draw = function () {
-    this.computeOptimumLocation();
     console.log("Drawo");
   };
 }
 
+
 let circle = new Circle(10);
+circle.getDefaultLocation();
+circle.draw();
