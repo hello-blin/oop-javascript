fc59d75a49dd37b00b59760aca7e109834d9f135
Blin Kukaj <kblin164@gmail.com>
2022-04-10 02:00:53 +0200

    Adding and removing properties

--- adding-and-removing-properties/index.js
+++ adding-and-removing-properties/index.js
@@ -1,12 +1,18 @@
 function createCircle(radius) {
-  return  {
+  return {
     radius,
-    draw: function(){
-        console.log('draw');
-    }
+    draw: function () {
+      console.log("draw");
+    },
   };
 }
 
-const circle = createCircle(10)
+const circle = createCircle(10);
 
-circle.location = {x: 1};
+const propertyName = "location";
+
+circle.location = { x: 1 }; //adding a new property by the key called location and it's value 1
+circle["center"] = { u: 1 }; //another way adding a new property by the key called location and it's value 1 using brackets
+// circle[propertyName] = {x: 1} //A special case using brackets is when we have a special
+
+delete circle['center']; //Removing a property we use delete keyword before and we can use one of the 3 methods I showed you up.
