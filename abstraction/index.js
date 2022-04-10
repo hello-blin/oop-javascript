5b2ca9505cc09f8ff25fde65917fde38c756672d
Blin Kukaj <kblin164@gmail.com>
2022-04-10 03:18:39 +0200

    Abstraction => Show the essentials and hide the details

---
+++ abstraction/index.js
@@ -0,0 +1,14 @@
+function Circle(radius) {
+  this.radius = radius;
+
+  this.defaultLocation = {x: 0, y: 0};
+  this.computeOptimumLocation = function(){
+
+  };
+  this.draw = function () {
+      this.
+    console.log("Drawo");
+  };
+}
+
+let circle = new Circle(10)
